93eb75947cb0c490aa3750c9d53fbeccc1f9889c
Will Butler <hatten33@users.noreply.github.com>
2015-02-24 09:06:34 -0800

    javadoc

--- src/main/java/net/aerenserve/starport/Main.java
+++ src/main/java/net/aerenserve/starport/Main.java
@@ -3,6 +3,13 @@ package net.aerenserve.starport;
 import net.aerenserve.starport.engine.StarPortSimulator;
 
 public class Main {
+	
+	/**
+	 * Launch a StarPortSimulator instance from command line
+	 * 
+	 * @param args As of now, no args are needed to run.
+	 * @see StarPortSimulator
+	 */
 
 	public static void main(String[] args) {
 		System.out.println("Starting StarPort Simulator. . .");
